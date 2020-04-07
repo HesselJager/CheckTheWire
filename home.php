b2a4e60b16aa536a2081aebc62cf34ef107a4d4f
HesselJager <h.f.jager@st.hanze.nl>
2020-04-07 05:25:17 +0200

    Initial Commit

---
+++ home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <link rel="icon" href="./images/CheckTheWireLogo.png">
+    <title>Check The Wire</title>
+</head>
+<body>
+<p>test</p>
+</body>
+</html>
