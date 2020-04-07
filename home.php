d6a76aa6e4db885263e177bb6862a73b45e3bccc
HesselJager <h.f.jager@st.hanze.nl>
2020-04-07 08:22:58 +0200

    Beginnings done

--- home.php
+++ home.php
@@ -1,10 +1,146 @@
 <!DOCTYPE html>
-<html lang="en-us">
+<html lang="en">
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Logo and title of the site -->
     <link rel="icon" href="./images/CheckTheWireLogo.png">
     <title>Check The Wire</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+    <style>
+        html,
+
+        head,
+
+        body {
+            background-color: #3d3b3c;
+        }
+
+        td {
+            vertical-align: bottom;
+            horiz-align: left;
+            width: 400px;
+            height: 125px;
+        }
+
+        #header {
+            min-height: 270px;
+            background-color: #3d3b3c;
+        }
+
+        .button {
+            background-color: white;
+            color: #3d3b3c;
+            border-radius: 4px;
+            border: 2px solid white;
+            transition-duration: 0.4s;
+            width: 100%;
+            height: 100%;
+        }
+
+        .button:hover {
+            background-color: #3d3b3c;
+            color: white;
+        }
+
+    </style>
 </head>
+
 <body>
-    <p>test</p>
+
+    <div id="header" align="center" style="padding-top: 10px">
+        <a href="https://twitter.com/checkthewire" target="_blank">
+            <img src="./images/CheckTheWireLogo.png" alt="Check The Wire logo" width="250" height="250">
+        </a>
+    </div>
+
+    <div>
+        <table align="center" cellpadding="29">
+            <tr>
+                <td>
+                    <a href="https://podcasts.apple.com/us/podcast/check-the-wire/id1500194762" target="_blank">
+                        <button class="button">
+                            <img src="./images/ItunesLogo.png" height="50"><b>iTunes</b>
+                        </button>
+                    </a>
+                </td>
+                <td>
+                    <a href="https://open.spotify.com/show/21qAykidhyxuSUu6ITVkPu?si=F5CXXtNNQ4C7OJVgN332BQ" target="_blank">
+                        <button class="button">
+                            <img src="./images/SpotifyLogo1.png" height="50"><b>Spotify</b>
+                        </button>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://podcasts.google.com/?feed=aHR0cHM6Ly9jaGVja3RoZXdpcmUubGlic3luLmNvbS9yc3M%3D" target="_blank">
+                        <button class="button">
+                            <img src="./images/GooglePodcastsLogo.png" height="50"><b>Google Podcasts</b>
+                        </button>
+                    </a>
+                </td>
+                <td>
+                    <a href="https://play.google.com/music/listen?u=0#/ps/I6uarxlavw4k5jsdh46hywnequu" target="_blank">
+                        <button class="button">
+                            <img src="./images/GooglePlayMusicLogo.png" height="50"><b>Google Play Music</b>
+                        </button>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://www.stitcher.com/podcast/check-the-wire?refid=stpr" target="_blank">
+                        <button class="button">
+                            <img src="./images/StitcherLogo.png" height="50"><b>Stitcher</b>
+                        </button>
+                    </a>
+                </td>
+                <td>
+                    <a href="https://soundcloud.com/dan-gheesling/sets/check-the-wire" target="_blank">
+                        <button class="button">
+                            <img src="./images/SoundcloudLogo.png" height="50"><b>SoundCloud</b>
+                        </button>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://tunein.com/podcasts/Media--Entertainment-Podcasts/Check-The-Wire-p1300596/" target="_blank">
+                        <button class="button">
+                            <img src="./images/TuneInLogo.png" height="50"><b>TuneIn Radio</b>
+                        </button>
+                    </a>
+                </td>
+                <td>
+                    <a href="https://www.youtube.com/playlist?list=PL1bauNEiHIgyareu5yHZqxT_uBjaO5KCw" target="_blank">
+                        <button class="button">
+                            <img src="./images/YouTubeLogo.png" height="50"><b>YouTube</b>
+                        </button>
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <!-- jQuery -->
+    <!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>-->
+    <!-- Popper.js -->
+    <!--<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>-->
+    <!-- Bootstrap JS -->
+    <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>-->
 </body>
 </html>
